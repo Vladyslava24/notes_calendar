56ba7fd1419c18d4498eb49939e17f439b7bad31
Vladyslava24 <vlada.lada.17@gmail.com>
2021-06-14 23:38:45 +0300

    Add flutter picker for time interface and reformat basic input form for notes

--- lib/widget/note_form_widget.dart
+++ lib/widget/note_form_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/Picker.dart';
 
 class NoteFormWidget extends StatelessWidget {
   final String? description;
@@ -11,14 +12,35 @@ class NoteFormWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SingleChildScrollView(
-    child: Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(/*
-            children: [
+  Widget build(BuildContext context) => Row(
+    //child: Padding(
+      //padding: EdgeInsets.all(16),
+      //child: ListView(
+    textDirection: TextDirection.ltr,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    verticalDirection: VerticalDirection.up,
+    mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+            child: ListTile(
+                title: Text('00.00'),
+            onTap: () {
+              showPickerNumberFormatValue(context);
+            },
+          ),
+          ),
+            Expanded(child:
+            //TextFormField(
+              //title: Text('Add new note'),
+              //onTap: () {
+                buildDescription(),
+             // },
+            //),
+            ),
+        //mainAxisSize: MainAxisSize.min,
+        //children: [
+          //Row(
+            /*children: [
               Switch(
                 value: isImportant ?? false,
                 onChanged: onChangedImportant,
@@ -33,14 +55,14 @@ class NoteFormWidget extends StatelessWidget {
                 ),
               )
             ], */
-          ),
+          //),
           //buildTitle(),
-          SizedBox(height: 8),
-          buildDescription(),
-          SizedBox(height: 16),
+          //SizedBox(height: 8),
+          //buildDescription(),
+          //SizedBox(height: 16),
         ],
-      ),
-    ),
+      //),
+    //),
   );
 
   /*Widget buildTitle() => TextFormField(
@@ -60,6 +82,39 @@ class NoteFormWidget extends StatelessWidget {
     title != null && title.isEmpty ? 'The title cannot be empty' : null,
     onChanged: onChangedTitle,
   );*/
+
+  showPickerNumberFormatValue(BuildContext context) {
+    Picker(
+        adapter: NumberPickerAdapter(data: [
+          NumberPickerColumn(
+              begin: 0,
+              end: 23,
+              onFormatValue: (v) {
+                return v < 10 ? "0$v" : "$v";
+              }
+          ),
+          NumberPickerColumn(begin: 0, end: 60,
+              onFormatValue: (v) {
+                return v < 10 ? "0$v" : "$v";
+              }
+          ),
+        ]),
+        delimiter: [
+          PickerDelimiter(child: Container(
+            width: 30.0,
+            alignment: Alignment.center,
+            child: Icon(Icons.more_vert),
+          ))
+        ],
+        hideHeader: true,
+        title: Text("Please Select"),
+        selectedTextStyle: TextStyle(color: Colors.blue),
+        onConfirm: (Picker picker, List value) {
+          print(value.toString());
+          print(picker.getSelectedValues());
+        }
+    ).showDialog(context);
+  }
 
   Widget buildDescription() => TextFormField(
     maxLines: 5,
